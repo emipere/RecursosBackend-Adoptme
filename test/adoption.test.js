1760117ca49f0e298abf07b6484e54cf5f2f6790
emipere <emipereiro@gmail.com>
2025-05-28 15:42:04 -0300

    preparando final

---
+++ test/adoption.test.js
@@ -0,0 +1,12 @@
+import supertest from "supertest";
+
+import { expect } from "chai";
+
+const requester = supertest("http://localhost:8080");
+
+describe("Testing de la App Web Adoptame", () => {
+    describe("Testing de Adopciones", () => {
+       
+    })
+})
+
